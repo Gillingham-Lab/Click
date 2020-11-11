3f406ec38cd5260d7d6caabb8cd7095f36a9cf00
Vassyli <basilius.sauter@gmail.com>
2020-11-11 19:26:21 +0100

    Adds setup.py with requirements.

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ClickReaction", # Replace with your own username
+    version="0.1.0",
+    author="Basilius Sauter",
+    author_email="basilius.sauter@gmail.com",
+    description="A collection of chemical reaction formulations for use with rdkit.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Gillingham-Lab/Click",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "rdkit",
+    ]
+)
