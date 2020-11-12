0693c4f899ba8c135b48330c5528b4ad764d47ec
Basilius Sauter <basilius.sauter@gmail.com>
2020-11-12 10:38:36 +0100

    Removes a unnecessary comment.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ClickReaction", # Replace with your own username
+    name="ClickReaction",
     version="0.3.0",
     author="Basilius Sauter",
     author_email="basilius.sauter@gmail.com",
